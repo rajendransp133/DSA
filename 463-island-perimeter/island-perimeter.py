c118bc7a2d4c5555c9739fabfda1fc9f915ee005
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-17 21:18:13 +0530

    Time: 86 ms (42.36%) | Memory: 19.7 MB (62.29%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,6 +1,5 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        grid_copy = [[i * 4 for i in row] for row in grid]
         m=len(grid)
         n=len(grid[0])
 
@@ -11,11 +10,12 @@ class Solution:
         for row in range(m):
             for j in range(n):
                 if(grid[row][j]>0):
+                    dum_sum=4
                     for k in check:
                         if(0<=row+k[0]<m and 0<=j+k[1]<n):
                             if(grid[row+k[0]][j+k[1]]>0):
-                                grid_copy[row][j]-=1
-                perimeter+=grid_copy[row][j]
+                                dum_sum-=1
+                    perimeter+=dum_sum
 
         return perimeter
 
