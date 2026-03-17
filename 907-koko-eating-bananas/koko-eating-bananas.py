2b898cea69d9d638c52631e31b8ad2ff160c5920
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-17 07:34:02 +0530

    Time: 589 ms (5.02%) | Memory: 20.6 MB (72.63%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,24 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        ceil_left=math.ceil(sum(piles)/h)
+        ceil_right = math.ceil(max(piles) * h ) +1
+
+        def check_func(div):
+            piles_2=piles.copy()
+            output_iterator = map(lambda x : math.ceil(x/div), piles_2)
+            if(sum(list(output_iterator))<=h):
+                return True
+            else:
+                return False
+        curr_global=-1
+
+        while(ceil_left<ceil_right):
+            mid = (ceil_left + ceil_right) //2
+            if(check_func(mid)):
+                curr_global=mid
+                ceil_right=mid
+            else:
+                ceil_left=mid+1
+
+        return curr_global
