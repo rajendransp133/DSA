67f2fa8212b608ae748ada692568b136b2029c16
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-21 09:48:08 +0530

    Time: 9 ms (98.37%) | Memory: 19.3 MB (94.64%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,29 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        hashmap={}
+        freq_arr=[0]*26
+        freq_arr2=[0]*26
         for i in s1:
-            if(i in hashmap):
-                hashmap[i]+=1
-            else:
-                hashmap[i]=1
-        hashmap2=hashmap.copy()
-        for i in range(len(s2)):
-            if(s2[i] in hashmap):
-                hashmap[s2[i]]-=1
-                if(hashmap[s2[i]]==0):
-                    del hashmap[s2[i]]
-                j=i+1
-                while(j<len(s2) and s2[j] in hashmap):
-                    hashmap[s2[j]]-=1
-                    if(hashmap[s2[j]]==0):
-                        del hashmap[s2[j]]
-                        
-                    j+=1
-            if(len(hashmap)==0):
+            freq_arr[ord(i)-97]+=1
+        fixed_window=len(s1)
+        if(len(s2)>=fixed_window):
+            for j in range(fixed_window):
+                freq_arr2[ord(s2[j])-97]+=1
+            if(freq_arr==freq_arr2):
                 return True
-            else:
-                hashmap=hashmap2.copy()
+            print(fixed_window)
+            for k in range(fixed_window,len(s2)):
+                freq_arr2[ord(s2[k-fixed_window])-97]-=1
+                freq_arr2[ord(s2[k])-97]+=1
+                if(freq_arr==freq_arr2):
+                    return True
         return False
+
+
+
+           
+        
 
 
 
