d06298a9af91b37bbf7e7ae7870504439ad7158c
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-23 18:05:28 +0530

    Time: 71 ms (1.29%) | Memory: 19.2 MB (96.88%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        final_return_list=[[]]
+
+        def recursion(arr,num):
+            final_return_list_updated=[]
+            for j in arr:
+                final_return_list_updated.append(j.copy())
+                j.append(num)
+                final_return_list_updated.append(j)
+                print(final_return_list_updated)
+            return final_return_list_updated
+
+        for i in nums:
+            final_return_list=recursion(final_return_list,i)
+
+        return final_return_list
+
