b0f54b547e1d085eb55dd0f7d366709666b4a1f3
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-27 17:22:30 +0530

    Time: 10 ms (14.50%) | Memory: 23.5 MB (58.06%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        hashmap={5:0,10:0,20:0}
+        for i in bills:
+            if(i==5):
+                hashmap[5]+=1
+            if(i==10):
+                if(hashmap[5]==0):
+                    return False
+                else:
+                    hashmap[10]+=1
+                    hashmap[5]-=1
+            if(i==20):
+                if(hashmap[10]>0 and hashmap[5]>0):
+                    hashmap[10]-=1
+                    hashmap[5]-=1
+                elif(hashmap[10]==0 and hashmap[5]>=3):
+                    hashmap[5]-=3
+                else:
+                    return False
+        return True
+
+        
