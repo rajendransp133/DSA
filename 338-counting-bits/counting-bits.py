2b8fb2b900a57bf8c325a79e0445d086f832de42
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-29 17:27:52 +0530

    Time: 5 ms (57.04%) | Memory: 20.2 MB (59.03%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -5,9 +5,12 @@ class Solution:
         if(n==1):
             return [0,1]
         return_arr=[0,1]
+        curr_floor=1
         for i in range(2,n+1):
-            floor_log= math.floor(math.log2(i))
-            return_arr.append(return_arr[i%(2**floor_log)]+1)
+            if(i%curr_floor == 0):
+                curr_floor*=2
+            return_arr.append(return_arr[i%curr_floor]+1)
+
         return return_arr
 
 
