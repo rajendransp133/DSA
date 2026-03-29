2f93a210f4155d7324ce9c6836b891579c93f96b
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-29 15:40:29 +0530

    Time: 22 ms (46.96%) | Memory: 19.6 MB (81.66%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -5,27 +5,22 @@ class Solution:
         checks=[(0,1),(0,-1),(1,0),(-1,0)]
         m=len(grid)
         n=len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if(grid[i][j]==0):
-                    visited_set.add((i,j))
         
         for i in range(m):
             for j in range(n):
-                count=0
                 if(grid[i][j]==1 and (i,j) not in visited_set):
                     Q=[(i,j)]
-                    maxx_area=max(1,maxx_area)
+                    visited_set.add((i,j))
+                    count=1
                     while(Q):
                         return_val=Q.pop(0)
-                        print(return_val,Q)
                         for k in checks:
                             x=return_val[0]+k[0]
                             y=return_val[1]+k[1]
-                            if((0<=x<m) and (0<=y<n) and (x,y) not in visited_set):
+                            if((0<=x<m) and (0<=y<n) and grid[x][y]==1 and (x,y) not in visited_set):
                                 Q.append((x,y))
                                 visited_set.add((x,y))
                                 count+=1
-                maxx_area=max(maxx_area,count)
+                    maxx_area=max(maxx_area,count)
 
         return maxx_area
