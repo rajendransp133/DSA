716a480f26d31ca6535dbd4c706c0dc26b36ae73
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2026-03-29 17:29:20 +0530

    Time: 8 ms (34.97%) | Memory: 20.4 MB (16.53%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,10 +1,10 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        return_arr=[0,1]
         if(n==0):
             return [0]
         if(n==1):
-            return [0,1]
-        return_arr=[0,1]
+            return return_arr
         curr_floor=1
         for i in range(2,n+1):
             offset=i%curr_floor
